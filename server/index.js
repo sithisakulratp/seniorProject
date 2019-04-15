c536b94cd4aa5334ca170f291e965df84a7a2172
Ploy Sithisakulrat <ploy.sitis@gmail.com>
2019-04-15 00:02:09 -0400

    add routes to all htmls files

--- server/index.js
+++ server/index.js
@@ -1,5 +1,36 @@
-const app = require('express')()
-app.get('/api', (req, res) => {
-  res.send('Hello, World!')
-})
+const app = require('express')();
+const path = require('path');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname will resolve to the folder of the file
+});
+
+router.get('/api', (req, res) => {
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+router.get('/demographics', (req, res) => {
+  res.sendFile(path.join(__dirname+'/demographics.html'))
+});
+
+router.get('/intersectionality', (req, res) => {
+  res.sendFile(path.join(__dirname+'/intersectionality.html'))
+});
+
+router.get('/comparison', (req, res) => {
+  res.sendFile(path.join(__dirname+'/comparison.html'))
+});
+
+router.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname+'/about.html'))
+});
+
+// store all html files in view folder
+app.use(express.static(__dirname + '/View'));
+// store all js and css files in scripts folder
+app.use(express.static(__dirname + 'Script'));
+
+app.use('/', router);
 app.listen()
